06648db9bcce90e5c90d0d87b43090e6c00e2343
Ribosome2 <583154233@qq.com>
2024-05-14 07:56:48 +0800

    登录流程（WIP)加入数据库组件

--- Server/Hotfix/Demo/Realm/FiberInit_Realm.cs
+++ Server/Hotfix/Demo/Realm/FiberInit_Realm.cs
@@ -16,6 +16,8 @@ namespace ET.Server
             StartSceneConfig startSceneConfig = StartSceneConfigCategory.Instance.Get(root.Fiber.Id);
             root.AddComponent<NetComponent, IPEndPoint, NetworkProtocol>(startSceneConfig.InnerIPPort, NetworkProtocol.UDP);
 
+            root.AddComponent<DBManagerComponent>();
+
             await ETTask.CompletedTask;
         }
     }
